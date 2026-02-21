26ed82562ccf9a4786dc0cb37366f4e33b96728e
aishsampathb <aishsampathb@gmail.com>
2026-02-21 22:32:13 +0530

    ep2 react fn component

--- ep1/App.js
+++ ep1/App.js
@@ -16,14 +16,30 @@ import ReactDOM from "react-dom/client";
 //     React.createElement("h1", {}, `I'm h1 tag`)
 //   )
 // );
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, `I'm h1 tag`),
-    React.createElement("h2", {}, `I'm h2 tag`),
-  ])
-);
-console.log(parent);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, `I'm h1 tag`),
+//     React.createElement("h2", {}, `I'm h2 tag`),
+//   ])
+// );
+// console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+const elem = <h1> el</h1>;
+const title = <div>title {elem}</div>;
+const Comp2 = () => <h1>Comp2</h1>;
+function HeadingComponent() {
+  return (
+    <div>
+      Heading
+      {Comp2()}
+      {title}
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
